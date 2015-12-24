389820ea754464c23935a20ecd6e97de6cb3df81
Milan Felix Sulc <rkfelix@gmail.com>
2015-12-24 13:19:04 +0100

    Bump

---
+++ README.md
@@ -0,0 +1,36 @@
+# xHosting Tools
+
+All tools in one place
+
+## Install
+
+```sh
+composer create-project f3l1x/xhosting
+```
+
+## Tools
+
+### PHP 
+
+- phpinfo
+
+### PHP OpCache
+
+- [OCP](https://gist.github.com/ck-on/4959032) [0.1.7]
+- [OpCache GUI](https://github.com/amnuts/opcache-gui) [master]
+- [OpCache Status](https://github.com/rlerdorf/opcache-status) [2.1.0]
+
+### Database
+
+- [Adminer](https://github.com/vrana/adminer) [4.2.3]
+- [Adminer Custom](https://github.com/dg/adminer-custom) [1.6.4]
+- [WWW SQL Designer](https://github.com/ondras/wwwsqldesigner) [master]
+
+### Nette
+
+- [Requirements Checker](https://github.com/nette/nette/tree/v2.3.8/tools/Requirements-Checker) [2.3.8]
+
+------
+
+Thank to all authors for all great addons and tools.
+
---
+++ index.php
@@ -0,0 +1,47 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>(x)Hosting Tools</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous">
+  </head>
+
+  <body>
+    <div class="container">
+
+      <div class="page-header">
+        <h1>(x)Hosting Tools</h1>
+        <p class="lead">All tools in one place.</p>
+      </div>
+
+      <h3>PHP</h3>
+      <ul>
+      	<li><a href="php/info.php">phpinfo()</a></li>
+      </ul>
+
+      <h3>PHP OpCache</h3>
+      <ul>
+        <li><a href="php/opcache/ocp/index.php">OCP</a> [0.1.7]</li>
+        <li><a href="php/opcache/gui/index.php">OpCache GUI</a> [master]</li>
+        <li><a href="php/opcache/status/index.php">OpCache Status</a> [2.1.0]</li>
+      </ul>
+
+      <h3>Database</h3>
+      <ul>
+        <li><a href="db/adminer/">Adminer</a> [4.2.3]</li>
+        <li><a href="db/adminer-custom/">Adminer Custom</a> [1.6.4]</li>
+        <li><a href="db/sqldesigner/">SqlDesigner</a> [master]</li>
+      </ul>
+
+      <h3>Nette</h3>
+      <ul>
+      	<li><a href="nette/checker/">Requirements Checker</a> [2.3.8]</li>
+      </ul>
+
+    </div>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" crossorigin="anonymous"></script>
+  </body>
+</html>
